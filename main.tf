5c0ee5d482f2f17ff43475502dbb2c5efa0d861f
Devops <155249106+MadhuMidimala@users.noreply.github.com>
2024-08-31 22:41:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "Terraform-EC2"
-  }
+provider "aws" {
+access_key = "xxxxxxxxxxxxxxx"
+secret_key = "xxxxxxxxxxxxxxxxxxxxx"
+region = "us-east-2"
+}
+resource "aws_instance" "Instance-01" {
+ami = "var.ami_id"
+instance_type = "var.instance_type"
+tags = {
+Name = "Instance-01"
+}
 }
